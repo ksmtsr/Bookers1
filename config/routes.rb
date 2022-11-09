383792bfccfe386dacb557fb1005bf5e1ffc4615
kamtsr <116964080+ksmtsr@users.noreply.github.com>
2022-11-09 10:10:08 +0000

    [Start]Bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  root :to => 'homes#top'
+  get 'top' => 'homes#top'
   resources :books
 
 
